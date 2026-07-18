bd5404512a9771d0dd231041e53695e642d7b5c8
twist <s.bonnet230906@gmail.com>
2026-07-18 16:33:48 +0200

    🚀 feat: Setup simple Main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,29 @@
+mod card;
+mod game;
+mod player;
+mod action;
+mod moment;
+
+use game::Game;
+use player::Player;
+
 fn main() {
     println!("Hello, world!");
+    println!("Play Poker !");
+
+    const BANKROLL: i32 = 200;
+
+    // setup player
+    let player1 = Player::new(1, "Player1".to_string(), BANKROLL);
+    let player2 = Player::new(2, "Player2".to_string(), BANKROLL);
+    let player3 = Player::new(3, "Player3".to_string(), BANKROLL);
+    let player4 = Player::new(4, "Player4".to_string(), BANKROLL);
+
+
+    // init game + link players
+    let mut game = Game::new();
+    game.add_player(player1);
+    game.add_player(player2);
+    game.add_player(player3);
+    game.add_player(player4);
 }
