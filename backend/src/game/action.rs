cbb982a0a3f8b25d90a753165e82f97a2da07063
twist <s.bonnet230906@gmail.com>
2026-07-26 22:05:58 +0200

    💯 feat: Add All-in functionality

--- backend/src/game/action.rs
+++ backend/src/game/action.rs
@@ -6,4 +6,5 @@ pub enum Action {
     Check,
     Call,
     Raise(i32),
+    AllIn,
 }
--- backend/src/game/moment.rs
+++ backend/src/game/moment.rs
@@ -16,16 +16,6 @@ impl Moment{
             Moment::Preflop => Moment::Flop,
             Moment::Flop => Moment::Turn,
             Moment::Turn => Moment::River,
-            Moment::River => Moment::River,
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn reset(&self) -> Self {
-        match self{
-            Moment::Preflop => Moment::Preflop,
-            Moment::Flop => Moment::Preflop,
-            Moment::Turn => Moment::Preflop,
             Moment::River => Moment::Preflop,
         }
     }
